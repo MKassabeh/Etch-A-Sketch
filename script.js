99c020203f086e0fca2d81b3e556ef9e088f3453
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-01-06 11:46:06 +0100

    function grid creation

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+    cell.addEventListener('mouseover', (event) => {
+        cell.style.backgroundColor = "red";
+    });
+  };
+
+};
+
+
+
+makeRows(16, 16);
