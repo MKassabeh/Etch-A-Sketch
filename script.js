05484943f2fc1e3717330365f17e4fdfda0838d8
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-01-06 13:29:51 +0100

    add clear button & range input number squares

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 const container = document.getElementById("container");
+const input = document.getElementById("sideSlider");
+const sizeValue = document.getElementById("sizeValue");
+const inputValue = +input.value;
+const clear = document.getElementById("clear");
+
+
+sizeValue.textContent = input.value
+input.addEventListener("input", (event) => {
+  sizeValue.textContent = event.target.value
+})
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
+    
+  
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
+    
+    clear.addEventListener("click", () =>{
+        cell.style.backgroundColor = "white";
+    })  
     cell.addEventListener('mouseover', (event) => {
-        cell.style.backgroundColor = "red";
-    });
-  };
+        let red = Math.floor(Math.random()*256 );
+        let blue = Math.floor(Math.random()*256 );
+        let green = Math.floor(Math.random()*256 );
+        cell.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+    })
+  } 
+
+
 
 };
 
 
 
-makeRows(16, 16);
+makeRows(inputValue, inputValue);
