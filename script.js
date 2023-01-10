6aac04f852e5e86638cda8660d95410176874e98
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-01-10 18:10:05 +0100

    function choice number cells

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const container = document.getElementById("container");
 const input = document.getElementById("sideSlider");
 const sizeValue = document.getElementById("sizeValue");
-const inputValue = +input.value;
+
 const clear = document.getElementById("clear");
 
-
 sizeValue.textContent = input.value
-input.addEventListener("input", (event) => {
-  sizeValue.textContent = event.target.value
-})
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    
-  
-    let cell = document.createElement("div");
+
+function makeRows(size) {
+
+
+  for (c = 0; c < (size* size); c++) {
+
+    let cell = document.createElement("span");
     container.appendChild(cell).className = "grid-item";
+    container.style.gridTemplateColumns = "repeat("+size+",1fr)";
+    container.style.gridTemplateRows = "repeat("+size+",1fr)";
     
     clear.addEventListener("click", () =>{
         cell.style.backgroundColor = "white";
@@ -33,7 +31,12 @@ function makeRows(rows, cols) {
 
 
 };
+input.addEventListener("input", (event) => {
+    sizeValue.textContent = event.target.value
+    input.value = sizeValue.textContent;
+    var inputValue = sizeValue.textContent;
+  })
 
 
-
-makeRows(inputValue, inputValue);
+  makeRows(28,28);
+  
